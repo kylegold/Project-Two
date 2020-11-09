0f00765160685d98511fe2b825204833d4291005
kyle <kyle@kyle.nyc>
2020-11-09 16:40:44 -0500

    general folder/file structure setup with passport authentication for local use.. setting up database on heroku now

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": "quizApp_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+};
