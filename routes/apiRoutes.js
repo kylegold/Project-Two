c31dff98e69febbb1ecf2e67c839d83c16449435
kyle <kyle@kyle.nyc>
2020-11-11 11:15:35 -0500

    changed html routes to a router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,10 @@ const Router = require("express").Router();
 const quizRouter = require("./quizRouter.js");
 const passportRouter = require("./passportRouter.js");
 
+
 Router.use("/quizzes", quizRouter);
 
 Router.use("/", passportRouter);
+
 
 module.exports = Router;
