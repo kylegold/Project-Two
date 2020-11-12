00e069a6c08c52c9bff4fb1977935d00b2c445b2
roan1088 <rohan_188@yahoo.com>
2020-11-11 20:48:52 -0500

    Added way to access option values

--- public/js/create-quiz.js
+++ public/js/create-quiz.js
@@ -10,9 +10,12 @@ $(document).ready(() => {
 
   // When more option button is clicked
   $("#more-option").on("click", function() {
-    console.log($(".option-info"));
-    // Clone an option row
-    $(".option-info:first").clone().addClass("cloned").appendTo(".all-options");
+    // console.log($(".option-info"));
+    // Clone an option row, making option empty and unchecking by default
+    const tempEl = $(".option-info:first").clone().addClass("cloned");
+    tempEl.find(".quiz_answer").val("");
+    tempEl.find(".answer-isCorrect").prop("checked", false);
+    tempEl.appendTo(".all-options");
     // Limit the options to 5
     if ($(".option-info").length === 5) {
       // Hide the more options button
@@ -24,7 +27,18 @@ $(document).ready(() => {
   $("#question-info").on("submit", function(event) {
     // Prevent form submission from refreshing the page
     event.preventDefault();
-    // console.log($("#quiz_question").val());
+
+    // Get all the options
+    const options = [];
+    for(let i = 0; i < $(".quiz_answer").length; i++) {
+      // console.log($($(".quiz_answer")[i]).val() + $($(".answer-isCorrect")[i]).prop("checked"));
+      options.push({
+        choice: $($(".quiz_answer")[i]).val(),
+        isCorrect: $($(".answer-isCorrect")[i]).prop("checked")
+      });
+    }
+    console.log(options);
+
     // Hide the form and reset it
     $("#question-info").hide();
     $("#question-info").trigger("reset");
