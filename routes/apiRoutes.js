ba06002a5b1cfca05fc26ba8e49c92031e591285
kyle <kyle@kyle.nyc>
2020-11-11 22:24:31 -0500

    add html routes for quiz id and user id search and quiz overiew and start the quiz... and linked buttons to navigate pages

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,13 @@
 const Router = require("express").Router();
 const quizRouter = require("./quizRouter.js");
 const passportRouter = require("./passportRouter.js");
-const quizWebApiRouter = require("./quizWebApiRouter.js");
+// const quizWebApiRouter = require("./quizWebApiRouter.js");
 
 Router.use("/", passportRouter);
 
 Router.use("/quizzes", quizRouter);
 
-Router.use("/quiz-web", quizWebApiRouter);
+// Router.use("/quiz-web", quizWebApiRouter);
 
 
 module.exports = Router;
--- routes/quizWebApiRouter.js
+++ routes/quizWebApiRouter.js
@@ -1,3 +1,28 @@
-const Router = require("express").Router();
+// const Router = require("express").Router();
+// const axios = require('axios');
 
-module.exports = Router;
+// const headers = {
+//   'api-key': '\',
+//   'Content-Type': 'application/json',
+// };
+
+// const config = {
+//   headers,
+//   baseUrl:  'https://quizapi.io/api/v1/questions/?apiKey=<api-key>',
+//   validateStatus(){
+// 	return true;
+//   }
+// };
+
+// const sendGetRequest = async () => {
+//     try {
+//         const resp = await axios.get('https://quizapi.io/api/v1/questions?category=linux', config);
+//         console.log(resp);
+//     } catch (err) {
+//         // Handle Error Here
+//         console.error(err);
+//     }
+// };
+
+// sendGetRequest();
+// module.exports = Router;
