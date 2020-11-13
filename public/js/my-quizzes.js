840a01e9797eb257ba249d3bc284844915d23561
kyle <kyle@kyle.nyc>
2020-11-13 12:05:29 -0500

    assigned value to li items of their respective quiz id

--- public/js/my-quizzes.js
+++ public/js/my-quizzes.js
@@ -8,15 +8,23 @@ async function populateQuizList(){
      const userData = await $.get("/api/user_data");
         // get user's quizzes
     const quizList = await $.get("/api/quizzes/user/"+ userData.id);
+    console.log(quizList)
         // for loop to concatenate li to the collection element in user-id-search
     var collection = $("#quiz-collection")
     for(var index=0; index<quizList.length; index= index+1){
         var quiz = quizList[index]; 
-        var url = "/take-a-quiz/quiz-overview/p/"+ quiz.id 
-        var li = '<a href="'+ url +'" class="collection-item">'+ quiz.title + ' | id: #' + quiz.id +'</a>'
+        var url = "/take-a-quiz/quiz-overview/"
+        var li = '<a href="#" class="collection-item" value="'+quiz.id+'">'+ quiz.title + ' | id: #' + quiz.id +'</a>'
+        console.log(quiz.id)
         collection.append(li); 
     }
-
+    // '<a href="'+ url +'
+    $(".collection-item").on("click", (event) => {
+        console.log($(event.target.getatrribute("value")))
+        // if(!sessionStorage.getItem('quizId')){
+        //     sessionStorage.getItem('quizId')
+        // }
+    })
     
 
 }
